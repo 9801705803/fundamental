b3116c2f37e72f2dc553934d50fd9a734045e725
9801705803 <prabin.karki5802@gmail.com>
2016-11-16 10:57:45 +0545

    Add files via upload
    Ask and print your name

---
+++ Ask and print.name.c
@@ -0,0 +1,18 @@
+/*
+Student Name:prabin karki
+Subject:Programming Fundamental
+Roll No :
+Lab sheet No:2
+Program:WAP to ask and print name
+Date:November 14,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char name[10];
+ printf("what is your name\n");
+ scanf("%s",&name);
+ printf("name of user is %s",name);
+ getch();
+}
