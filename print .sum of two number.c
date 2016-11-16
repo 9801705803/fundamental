fa06803e93ae0cc6c5a165d238ffaddf1aab2bef
9801705803 <prabin.karki5802@gmail.com>
2016-11-16 10:58:27 +0545

    Add files via upload
    WAP print sum of two number

---
+++ print .sum of two number.c
@@ -0,0 +1,20 @@
+/*
+Student Name :prabin karki
+Subject: Programming Fundamental
+Roll No:
+Program : WAP to print sum of two number ,asked from users
+Date: November 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+sum=a+b;
+printf("The sum of two number is %d",sum);
+getch();
+}
