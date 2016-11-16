6f7d6ea8a68ccd5040502080bcd3c3947d8655ed
9801705803 <prabin.karki5802@gmail.com>
2016-11-16 10:59:05 +0545

    Add files via upload
    WAP to product asked from user

---
+++ print.product of two number.c
@@ -0,0 +1,22 @@
+/*
+Student Name:Prabin karki
+Sunject:Programming Fundamental
+Roll No:
+Program:Wap to product of two number asked from user
+Date:November 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,product;
+printf("Enter the first number:\n");
+scanf("%d",&a);
+printf("Enter the second number:\n");
+scanf("%d",&b);
+product=a*b;
+printf("product of two number is : %d",product);
+getch();
+}
+
+
