663a108467046612c27c85a81725ba62aabe67d8
9801705803 <prabin.karki5802@gmail.com>
2016-11-16 12:24:26 +0545

    Add files via upload
    WAP to print division betn two number

---
+++ print.division of two number.c
@@ -0,0 +1,21 @@
+/*
+Student Name:Prabin Karki
+Subject:programming Fundamental
+Roll No:
+Lab sheet No:
+Program:Write a c program to print division of two numbers
+Date:November 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float a,b,division;
+ printf("Enter first number\n");
+ scanf("%f",&a);
+ printf("Enter second number\n");
+ scanf("%f",&b);
+ division=a/b;
+ printf("division of two number is : %f",division);
+ getch();
+}
