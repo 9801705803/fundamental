56dc4bf01b730bb9c9ec4758021f38f29c38da8e
9801705803 <prabin.karki5802@gmail.com>
2016-11-18 10:46:17 +0545

    Print your name
    print your name

--- print.c
+++ print.c
@@ -1,1 +1,14 @@
-
+/*
+Student Name :prabin karki
+Subject: Programming Fundamental
+Roll No:
+Program : WAP to print your name
+Date: November 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ printf("my name is prabin");
+ getch();
+}
