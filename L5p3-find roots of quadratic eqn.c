d1e870963cab88deae96081b86798a3243bbf13c
9801705803 <prabin.karki5802@gmail.com>
2016-11-23 12:14:07 +0545

    Add files via upload

--- L5p3-find roots of quadratic eqn.c
+++ L5p3-find roots of quadratic eqn.c
@@ -20,7 +20,7 @@ printf("Enter the value of c:\n");
 scanf("%f",&c);
 dis=(b*b)-(4*a*c);
 x1=(-b+pow(dis,0.5))/(2*a);
-printf("The value of x1 is : %f",x1);
+printf("The value of x1 is : %f\n",x1);
 x2=(-b-pow(dis,0.5))/(2*a);
 printf("The value of x2 is : %f",x2);
 getch();
