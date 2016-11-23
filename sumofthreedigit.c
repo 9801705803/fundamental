57d5b47fd5427d7ba4689df3c4e9f88a9eeb641d
9801705803 <prabin.karki5802@gmail.com>
2016-11-23 10:19:34 +0545

    L5p1-sum of 3 digit
    L5p1-sum of 3 digit

---
+++ sumofthreedigit.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Prabin Karki
+Subject:programming Fundamental
+Roll No:11
+Lab Sheet No:5
+Program:Wap to find dum of digit of three digit number
+Date:November 23,2016
+*/
+
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum;
+printf("Enter the first digit number:\n");
+scanf("%d",&a);
+printf("Enter the second digit number:\n");
+scanf("%d",&b);
+printf("Enter the third digit number:\n");
+scanf("%d",&c);
+sum=a+b+c;
+printf("The sum of three digit is : %d",sum);
+return 0;
+}
