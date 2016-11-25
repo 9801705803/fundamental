5f0b1cbd05043f70c10f6e16f07253ca74cdf34a
9801705803 <prabin.karki5802@gmail.com>
2016-11-25 10:24:46 +0545

    Add files via upload

---
+++ L6p2-Wap to check postive or negative.c
@@ -0,0 +1,31 @@
+/*
+Student Name:prabin karki
+Subject:Programming Fundamental
+Roll No :11
+Lab sheet No:6
+Program:Wap to check the no is positive and negative
+Date:November 25,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("Enter your number:\n");
+scanf("%d",&n);
+if (n>0)
+{
+printf("The number is positive");
+}
+else if(n<0)
+{
+ printf("The number is negative");
+}
+
+getch();
+return 0;
+}
+
+
+
+
