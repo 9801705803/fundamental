7035f709248a8dcaf32d89ccb79f24c190060e58
9801705803 <prabin.karki5802@gmail.com>
2016-11-28 11:18:57 +0545

    Add files via upload

---
+++ L7p2-Relational operators.c
@@ -0,0 +1,34 @@
+/*
+Student Name:Prabin Karki
+Subject:Programming Fundamental
+Roll No:11
+Lab Sheet No:7
+program:Write a c program to e
+Date:November 28,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b;
+ printf("Enter your first number:\n");
+ scanf("%d",&a);
+ printf("Enter your second number:\n");
+ scanf("%d",&b);
+ a==b;
+
+ printf("use of == %d\n", a==b);
+ a>b;
+ printf("use of > %d\n", a>b);
+ a<b;
+ printf("use of < %d\n",a<b);
+ a!=b;
+ printf("use of !=%d\n",a!=b);
+ a>=b;
+ printf("use of >=%d\n",a>=b);
+ a<=b;
+ printf("use of <=%d\n",a<=b);
+ getch();
+ return 0;
+}
