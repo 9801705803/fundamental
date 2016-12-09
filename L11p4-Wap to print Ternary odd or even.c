55d658c744746298ad8cbc1ceadfe30d58efb29b
9801705803 <prabin.karki5802@gmail.com>
2016-12-09 10:17:35 +0545

    Add files via upload

---
+++ L11p4-Wap to print Ternary odd or even.c
@@ -0,0 +1,20 @@
+/*
+student Name:Prabin karki
+Subject:Programming fundamental
+Roll No:22
+Lab sheet N0:10
+program:wap to print to use ternary
+Date:7 december,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ int n;
+ printf("Enter the number");
+ scanf("%d",&n);
+ (n%2==0)?printf("The number %d is even",n):printf("The number %d is odd",n);
+ getch();
+ return 0;
+ }
