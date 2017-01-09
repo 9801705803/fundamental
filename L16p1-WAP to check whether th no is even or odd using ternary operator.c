f56d25bdfb577c4316908c12898aea39159d7489
9801705803 <prabin.karki5802@gmail.com>
2017-01-09 10:46:53 +0545

    Add files via upload

---
+++ L16p1-WAP to check whether th no is even or odd using ternary operator.c
@@ -0,0 +1,20 @@
+/*
+Student Name:Prabin Karki
+Subject:Programming Fundamental
+Lab Sheet No:
+Roll No:26
+program:WAP to check whether no is even or odd using Ternary operator
+Date:Janaury 9,2017
+*/
+
+#include<stdio.h>
+
+ int main()
+ {
+  int a;
+  printf("Enter any number:\n");
+  scanf("%d",&a);
+  
+  (a%2==0)?printf("The number is even:\n"):printf("The number is odd:\n"):
+  return 0
+}
